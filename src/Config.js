821c7fb7482d42353b06104e80a2ddf92e88b7a7
newthuhole <newthuhole@gmail.com>
2025-04-30 18:47:32 +0800

    minor updates

--- src/Config.js
+++ src/Config.js
@@ -22,7 +22,7 @@ const BUILTIN_IMGS = {
 
 const DEFAULT_CONFIG = {
   background_img:
-    'https://cdn.jsdelivr.net/gh/thuhole/webhole@gh-pages/static/bg/gbp.jpg',
+    'https://www.tsinghua.edu.cn/image/nav-bg.jpg',
   background_color: '#113366',
   pressure: false,
   easter_egg: true,
